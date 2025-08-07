5fc8e1ac2b533aec0e560c62529533ebf850942e
Yuguan Wang <wangyuguan@hotmail.com>
2025-08-06 19:42:59 -0500

    clean up

--- src/optimization.py
+++ src/optimization.py
@@ -6,15 +6,10 @@ import jax.numpy as jnp
 from utils import * 
 from viewing_direction import * 
 from volume import * 
-import math 
 import time 
 from scipy.optimize import minimize
-from scipy.linalg import svd
-from aspire.volume import Volume 
-from aspire.numeric import fft
-from aspire.source.simulation import Simulation
 from tqdm import tqdm 
-from tqdm import trange
+
 
 
 
